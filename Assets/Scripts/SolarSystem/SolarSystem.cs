a4fde9f6ad7429db87f46be88b2bf3625f71a0dd
MalvinLH <malvinleo21@gmail.com>
2024-12-08 21:03:49 +0700

    feat: add audio

---
+++ Assets/Scripts/SolarSystem/SolarSystem.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarSystem : MonoBehaviour
+{
+    readonly float G = 100f;
+    GameObject[] celestials;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        celestials = GameObject.FindGameObjectsWithTag("Celestial");
+        InitialVelocity();
+    }
+
+    private void FixedUpdate()
+    {
+        Gravity();
+    }
+
+    void Gravity()
+    {
+        foreach (GameObject A in celestials)
+        {
+            foreach (GameObject B in celestials)
+            {
+                if (A != B)
+                {
+                    float m1 = A.GetComponent<Rigidbody>().mass;
+                    float m2 = B.GetComponent<Rigidbody>().mass;
+                    float r = Vector3.Distance(A.transform.position, B.transform.position);
+
+                    A.GetComponent<Rigidbody>().AddForce((B.transform.position - A.transform.position).normalized * (G * (m1 * m2) / (r * r)));
+                }
+            }
+        }
+    }
+
+    void InitialVelocity()
+    {
+        foreach (GameObject a in celestials)
+        {
+            foreach (GameObject b in celestials)
+            {
+                if (a != b)
+                {
+                    float m2 = b.GetComponent<Rigidbody>().mass;
+                    float r = Vector3.Distance(a.transform.position, b.transform.position);
+                    a.transform.LookAt(b.transform);
+
+                    a.GetComponent<Rigidbody>().velocity += a.transform.right * Mathf.Sqrt((G * m2) / r);
+                }
+            }
+        }
+    }
+}
